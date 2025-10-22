ede09f3f9ac340ff51acd480374aee83adda5f62
Louis Ouellet <louis_ouellet@hotmail.com>
2025-10-22 07:59:10 -0400

    Features: Updated the model to support the new count and joins methods.

--- Model.php
+++ Model.php
@@ -18,60 +18,17 @@ class UsersModel extends BaseModel {
     }
 
     /**
-     * Retrieve multiple records
+     * Apply Joins to the Query
      *
-     * @param array $conditions
-     * @return array
+     * @param Query $Query
+     * @return Query
      */
-    public function fetchAll(array $conditions = [], string $conjunction = 'AND'): array
+    protected function joins(object $Query): object
     {
-        // Create the Query
-        $Query = $this->Database->query()
-            ->table($this->table)
-            ->select('*')
-            ->join('owner', 'users', 'username')
-            ->join('vcard', 'vcards', 'id')
-            ->join('organization', 'organizations', 'id')
-            ->join('organization.vcard', 'vcards', 'id')
-            ->index($this->primary)
-            ->filter()
-            ->where('id', 9999, '<>')
-            ->where('organization', $this->Auth->user()->organization()->id);
+        // Apply Joins
+        $Query->join('vcard', 'vcards', 'id');
 
-        // Check if the conditions are empty
-        if(!empty($conditions)){
-
-            // Add a Filter
-            $Query->filter();
-
-            // Add the Conditions
-            foreach($conditions as $key => $condition){
-
-                // Check if the key exists in the definition
-                if(!array_key_exists($condition['key'], $this->definition)){
-
-                    // Remove the key from the data
-                    unset($conditions[$key]);
-                    continue;
-                }
-
-                // Add the condition to the Query
-                $Query->where($condition["key"], $condition["value"], $condition["operator"], $conjunction);
-            }
-        }
-
-        // Retrieve the Results
-        $records = $Query->fetch();
-
-        // Loop through the records to process them
-        foreach($records as $key => $record){
-
-            // Overwrite the record with the processed one
-            $records[$key] = $this->process($record);
-        }
-
-        // Return the Results
-        return $records;
+        return $Query;
     }
 
     /**
@@ -87,11 +44,9 @@ class UsersModel extends BaseModel {
             ->table($this->table)
             ->select('*')
             ->join('owner', 'users', 'username')
+            ->join('owner.vcard', 'vcards', 'id')
             ->join('backend', 'backends', 'id')
             ->join('session', 'sessions', 'id')
-            ->join('vcard', 'vcards', 'id')
-            ->join('organization', 'organizations', 'id')
-            ->join('organization.vcard', 'vcards', 'id')
             ->join('pin', 'pins', 'id')
             ->join('token', 'tokens', 'id')
             ->filter()
@@ -99,6 +54,16 @@ class UsersModel extends BaseModel {
             ->filter()
             ->where($this->primary, $id)
             ->limit(1);
+
+        // Set the Owner
+        if(array_key_exists('organization',$this->definition)){
+            $Query->join('organization', 'organizations', 'id')
+                ->join('organization.vcard', 'vcards', 'id')
+                ->where('organization', $this->Auth->user()->organization()->id);
+        }
+
+        // Apply Joins
+        $Query = $this->joins($Query);
 
         // Retrieve the record
         $records = $Query->fetch();
